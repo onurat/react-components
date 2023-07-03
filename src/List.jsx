dde2dd81802d18996f2ebf12175cbd02b022d4ce
Onur Atas <114289826+onurat@users.noreply.github.com>
2023-07-03 16:35:31 +0100

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>bacon</li>
+      <li>jamon</li>
+      <li>noodels</li>
+    </ul>
+  );
+}
+
+export default List;
